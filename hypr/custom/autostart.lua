dbc0c5f15d44e5f073ad571fe5d4141e15a14815
ulilab-dev <ullia5733@gmail.com>
2026-05-18 20:24:32 +0700

    Add swaybg command to autostart script

--- hypr/custom/autostart.lua
+++ hypr/custom/autostart.lua
@@ -10,5 +10,6 @@
  hl.on("hyprland.start", function () 
      hl.exec_cmd("nm-applet")
      hl.exec_cmd("waybar")
+     hl.exec_cmd("swaybg -i ~/.local/share/wallpapers/p3.jpg")
      hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
  end)
